fae53e948676f661b2f8793435af84b8aa5fcfb4
AustinKent <aakent99@gmail.com>
2025-11-26 22:37:59 -0500

    Playing with querySelectors

---
+++ app.js
@@ -0,0 +1,10 @@
+const pokemonName = document.querySelector("bh1")
+
+pokemonName.addEventListener("click", function(){
+    const containerEl = document.querySelector(".container")
+    // console.log(containerEl);
+    containerEl.style.backgroundColor = "hotpink"
+});
+
+// const backgroundEls = document.querySelectorAll(".pokemon-divs")
+//     console.log(backgroundEls);
