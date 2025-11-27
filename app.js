fe33b209dcc7123a12e1d5a927e83ba3f8f4234f
AustinKent <aakent99@gmail.com>
2025-11-27 10:39:18 -0500

    added click functionality

--- app.js
+++ app.js
@@ -1,10 +1,39 @@
-const pokemonName = document.querySelector("bh1")
+// Click on the h1 to change it to red
+const pokemonName = document.querySelector("h1")
 
 pokemonName.addEventListener("click", function(){
-    const containerEl = document.querySelector(".container")
-    // console.log(containerEl);
-    containerEl.style.backgroundColor = "hotpink"
+    pokemonName.style.color = "red"
 });
 
-// const backgroundEls = document.querySelectorAll(".pokemon-divs")
-//     console.log(backgroundEls);
+// Creating an array of random images, to exchange out when the image is clicked
+const image = document.querySelector("img")
+
+let links = [
+    "https://i.imgur.com/J7vfzJU.png", 
+    "https://i.imgur.com/ueGCmuu.png",
+    "https://i.imgur.com/kBckKQ0.png", 
+    "https://i.imgur.com/SLb20ez.png",
+    "https://i.imgur.com/LUuu8Nn.png"
+]
+
+// Randomly change the image using the links array
+image.addEventListener("click", function(){
+    // console.log("Image Click heard!")
+    let randomImg = Math.floor(Math.random() * links.length);
+    image.src = (links[randomImg])
+})
+
+// Creating a button function to toggle between light and dark mode
+const buttonEl = document.querySelector("button")
+
+const containerEl = document.querySelector(".container")
+
+// containerEl.addEventListener("click", function(){            //button click console log for testing purposes
+//     console.log("container click!")              
+// })
+
+buttonEl.addEventListener("click", function(){
+    // console.log("Button Click heard!")
+    containerEl.classList.toggle('lightMode')
+    // console.log(document.querySelector(".container"))
+})
